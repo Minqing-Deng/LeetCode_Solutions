b4c93eeba0e1f2551d696cf46d959e7a67985536
Minqing Deng <mdeng46@gatech.edu>
2024-12-27 21:02:13 -0800

    Daily update: 12/27/2024

--- Solutions/269_Alien_Dictionary.py
+++ Solutions/269_Alien_Dictionary.py
@@ -3,36 +3,40 @@ from typing import List
 
 class Solution:
     def foreignDictionary(self, words: List[str]) -> str:
-        adj = {c: set() for w in words for c in w}
+
+        order = {c: set() for w in words for c in w}
 
         for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            minLen = min(len(w1), len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+            w1 = words[i]
+            w2 = words[i + 1]
+            minLength = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLength] == w2[:minLength]:
                 return ""
-            for j in range(minLen):
+            for j in range(minLength):
                 if w1[j] != w2[j]:
-                    adj[w1[j]].add(w2[j])
+                    order[w1[j]].add(w2[j])
                     break
 
-        visited = {}
+        visit, cycle = set(), set()
         res = []
 
-        def dfs(char):
-            if char in visited:
-                return visited[char]
+        def dfs(c):
+            if c in cycle:
+                return False
+            if c in visit:
+                return True
 
-            visited[char] = True
+            cycle.add(c)
+            for nei in order[c]:
+                if not dfs(nei):
+                    return False
+            cycle.remove(c)
+            res.append(c)
+            visit.add(c)
+            return True
 
-            for neighChar in adj[char]:
-                if dfs(neighChar):
-                    return True
-
-            visited[char] = False
-            res.append(char)
-
-        for char in adj:
-            if dfs(char):
+        for c in order:
+            if not dfs(c):
                 return ""
 
         res.reverse()
